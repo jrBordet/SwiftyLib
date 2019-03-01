03f3227b21c7037dae28eb95ba082687bd182944
Jean Raphael Bordet <jr.bordet@touchwa.re>
2019-03-01 13:13:21 +0100

    Introduce test cases

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Jean Raphael Bordet on 01/03/2019.
+//  Copyright © 2019 Jean Raphael Bordet. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
